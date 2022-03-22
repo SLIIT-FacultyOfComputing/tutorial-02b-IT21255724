eafaa74c084245b6fc3156b9d2d705d9c2dbace0
IT21255724 <101991102+IT21255724@users.noreply.github.com>
2022-03-22 13:22:19 +0000

    codings

--- tute02.cpp
+++ tute02.cpp
@@ -12,17 +12,25 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+using namespace std;
 int main()
 {
-   double salary, netSalary;
-   int etype, otHrs, otRate;
-   printf("Enter Employee Type : ");
-   scanf("%d", &etype);
-   printf("Enter Salary  : ");
-   scanf("%f", &salary);
-   printf("Enter OtHrs : ");
-   scanf("%d", &otHrs); 
+  double salary, netSalary;
+  int etype, otHrs, otRate;
+  
+  cout<<"Enter Employee Type: ";
+  cin>>etype;
+
+  cout<<"Enter Salary: ";
+  cin>>salary;
+
+  cout<<"Enter OT Hours: ";
+  cin>>otHrs;
+
+   
+  
    
    switch (etype) {
       case 1 :
@@ -37,8 +45,8 @@ int main()
    }
 
 
-   netSalary = salary + otHrs* otRate;
-   printf("Net Salary is %f ", netSalary);
+   netSalary = salary + otHrs * otRate;
+   cout<<"Net Salary is: "<<setiosflags(ios::fixed)<<setprecision(1)<<netSalary;
   
    return 0;
 }
--- tute04.cpp
+++ tute04.cpp
@@ -12,18 +12,57 @@ long nCr(int n, int r);
 Do not modify the main function.*/
 
 #include <iostream>
+using namespace std;
 
 long Factorial(int no);
 long nCr(int n, int r);
 
 int main() {
   int n, r;
-  std::cout << "Enter a value for n ";
-  std::cin >> n;
-  std::cout << "Enter a value for r ";
-  std::cin >> r;
-  std::cout << "nCr = ";
-  std::cout << nCr(n,r);
-  std::cout << std::endl;
+  
+  cout<<"Enter a value for n ";
+  cin>>n;
+  
+  cout<<"Enter a value for r ";
+  cin>>r;
+  
+  cout<<"nCr = "<<nCr<<endl;
+
   return 0;
+}
+
+long Factorial(int no)
+{
+  int n, r, i;
+  int fac_n = 1;
+  int fac_r = 1;
+  int fac_n_minus_r = 1;
+  
+  for(n = i; i>=1; i--)
+  {
+    fac_n = fac_n * i;
+  }
+  return fac_r;
+  
+  for(r = i; i>=1; i--)
+  {
+    fac_r = fac_r * i;
+  }
+  return fac_r;
+
+  for((n-r) = i; i>=1; i--)
+  {
+    fac_n_minus_r = fac_n_minus_r * i;
+  }
+  return fac_n_minus_r;
+  
+}
+
+long nCr(int n, int r)
+{
+  int nCr
+
+  nCr = (fac_n)/(fac_r)*(fac_n_minus_r);
+
+  return nCr;
 }
